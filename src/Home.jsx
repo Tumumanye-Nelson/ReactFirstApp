52c253a5a8772052808af78dc3ae7cfb2d09bc88
Tumumanye-Nelson <tumumanyenelson@gmail.com>
2025-01-08 14:13:07 +0300

    added content in home component and some css

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,33 @@
-import React from "react";
-import photo from './assets/uganda.jpg'
-import Service from "./Service";
-import About from "./About";
-import Destination from "./Destination";
-import Tours from "./Tours";
-import Contact from "./Contact";
-import Blogs from "./Blogs";
+import photo from './assets/uganda.jpg';
 
-const Home = () =>{
-return(
-    <div className="H">
-        <h4>Uganda is the best tourism destination </h4>
-        <img src={photo} height={200} width={200} alt="uganda tourism" />
-
-        <div>
-            <p>we have the variety of tourism city in uganda </p>
-            <p> To know about the tourism destionation tap the below link </p>
-        </div>
-        <a className="img1" href="https://www.google.com/search?q=uganda+tourism+attractions&oq=uganda+tourism+attractions&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORiABDIICAEQABgWGB4yCAgCEAAYFhgeMggIAxAAGBYYHjIICAQQABgWGB4yCAgFEAAYFhgeMggIBhAAGBYYHjIICAcQABgWGB4yCAgIEAAYFhgeMg0ICRAAGIYDGIAEGIoF0gEINTQ0M2owajmoAgCwAgE&sourceid=chrome&ie=UTF-8" target="blank">LINK</a>
-<About/>
-<Blogs/>
-<Contact/>
-<Destination/>
-<Service/>
-
+const Home = () => {
+  return (
+    <div 
+      className="home-container" 
+      style={{
+        backgroundImage: `url(${photo})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        color: 'white'
+      }}
+    >
+      <h1 className='homeh4'>Uganda is the best tourism destination</h1>
+      <div className="description">
+        <p>We have a variety of tourism cities in Uganda.</p>
+        <p>To learn more about tourism destinations, click the link below.</p>
+      </div>
+      <a 
+        className="tourism-link" 
+        href="https://www.google.com/search?q=uganda+tourism+attractions" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        style={{ color: 'red', textDecoration: 'none' }}
+      >
+        LINK
+      </a>
     </div>
-);
-}
+  );
+};
+
 export default Home;
